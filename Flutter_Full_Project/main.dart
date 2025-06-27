ce120341a2ea05aec928a15e69dca2978aacd083
CMRIT <63945293+SarveswaraReddy-CMRIT@users.noreply.github.com>
2025-06-27 14:25:37 +0530

    Update main.dart

--- Flutter_Full_Project/main.dart
+++ Flutter_Full_Project/main.dart
@@ -1,1 +1,44 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_full_project/Login.dart';
+import 'dart:io';
 
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+  ? await Firebase.initializeApp(
+  options: const FirebaseOptions(
+  apiKey: "AIzaSyAlgZGNqKj-lOrK1ePbnWOEf3UcH5gJBpI",
+  appId: "1:629974825509:android:2c014cc0f57e4afe9bddcc",
+  messagingSenderId: "629974825509",
+  projectId: "flutter-full-project"),
+  )
+  :await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for Errors
+          if (snapshot.hasError) {
+            print("Something Went Wrong");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return MaterialApp(
+            title: 'Flutter Firebase EMail Password Auth',
+            theme: ThemeData(
+              primarySwatch: Colors.deepPurple,
+            ),
+            debugShowCheckedModeBanner: false,
+            home: Login(),
+          );
+        });
+  }
+}
